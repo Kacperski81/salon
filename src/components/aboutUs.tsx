eb1d910fdf2aff4f2f9f206e66b84221390a9c16
Kacperski81 <kacperski81@gmail.com>
2025-08-11 14:47:50 +0100

    e

--- src/components/aboutUs.tsx
+++ src/components/aboutUs.tsx
@@ -2,6 +2,7 @@ export default function AboutUs() {
     return (
         <div>
             <h2>About Us</h2>
+            <p>{`Nestled just off Putney High Street, a short stroll from the station, Infinity MK is your go-to salon for hair, nail, and beauty needs. We're dedicated to understanding your unique style and providing exceptional service to help you look great and feel amazing.`}</p>
         </div>
     )
 }
--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -8,10 +8,10 @@ export default function HeroSection() {
             {/* rgb(242, 231, 203) */}
             <div className="flex flex-col-reverse xl:grid xl:grid-cols-2 grow">
 
-                <h1 className="grow-1 font-(family-name:--font-aboreto) text-6xl flex flex-col pt-10 xl:justify-center items-center px-4 xl:px-16">
+                <h1 className="grow-1 font-(family-name:--font-aboreto) text-3xl lg:text-6xl flex flex-col pt-10 xl:justify-center items-center px-4 xl:px-16">
                     <span>FEEL GOOD, </span>
                     <span>LOOK AMAZING.</span>
-                    <span className="mt-4 font-normal text-lg">{`Step into a world of beauty and relaxation. We're here to make you shine`}</span>
+                    <span className="mt-4 font-normal text-lg">{`Step into a world of beauty and relaxation. We're here to make you shine.`}</span>
                 </h1>
                 <div className="bg-[#000] relative grow-3">
                     <Image
