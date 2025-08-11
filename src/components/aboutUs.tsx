a1c8aea29f5d66769d215363a0f3bd99ca91302a
Kacperski81 <kacperski81@gmail.com>
2025-08-11 14:27:28 +0100

    apost

--- src/components/aboutUs.tsx
+++ src/components/aboutUs.tsx
@@ -13,7 +13,7 @@ export default function AboutUs() {
                     style={{ objectFit: "cover" }}
                 />
             </div>
-            <div className="">
+            <div>
                 <h2>About us</h2>
                 <p>Nestled just off Putney High Street, a short stroll from the station, Infinity MK is your go-to salon for hair, nail, and beauty needs. We&#39;re dedicated to understanding your unique style and providing exceptional service to help you look great and feel amazing.</p>
             </div>
