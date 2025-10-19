7c97caac1ce547c57b4a5fa87341ee0e3131e990
Kacperski81 <kacperski81@gmail.com>
2025-10-19 19:52:52 +0100

    snap test8

--- src/app/hair/page.tsx
+++ src/app/hair/page.tsx
@@ -3,8 +3,14 @@ import { Border2 } from "@/components/border";
 import Footer from "@/components/footer";
 import Services from "@/components/hair/services";
 import Treatments from "@/components/hair/treatments";
+import {useLayoutEffect} from "react";
 
 export default function Hair() {
+
+    useLayoutEffect(() => {
+        window.scrollTo(0, 0);
+    },[])
+
     return (
         <div className="grow flex flex-col snap-y snap-start snap-always snap-proximity">
 
