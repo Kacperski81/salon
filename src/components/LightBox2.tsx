9994e9dc806ba5060205701292477ebc88fc514d
Kacperski81 <kacperski81@gmail.com>
2025-11-12 13:47:49 +0000

    debuggin2

--- src/components/LightBox2.tsx
+++ src/components/LightBox2.tsx
@@ -20,9 +20,9 @@ export default function LightBox({ open, item, items, onClose, handleOverlayClic
     if (!open || !item) return null;
 
     return (
-        <div className="fixed top-0 left-0 right-0 bottom-0 bg-(--main-800)/90 max-w-full max-h-[399px] z-50 flex justify-center items-center p-4" onClick={handleOverlayClick}>
+        <div className="fixed inset-0 bg-(--main-800)/90 max-w-full max-h-screen z-50 flex justify-center items-center p-4" onClick={handleOverlayClick}>
             <div className="relative">
-                <button aria-label="Close" className="absolute -top-8 -right-8 cursor-pointer z-100 w-10" onClick={onClose}><IconCloseCircle /></button>
+                <button aria-label="Close" className="absolute -top-6 -right-4 cursor-pointer z-100 w-10" onClick={onClose}><IconCloseCircle /></button>
                 <div className="bg-white shadow-lg">
                     <LightBoxCarousel items={items} item={item} />
                 </div>
